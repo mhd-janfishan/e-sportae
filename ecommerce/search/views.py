63b04ba48be60ff5da61c5dfc8afbfd56f6d2b6f
mhd-janfishan <mhdjanfishan21@gmail.com>
2022-10-06 18:42:49 +0530

    esportae

---
+++ ecommerce/search/views.py
@@ -0,0 +1,15 @@
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+from django.shortcuts import render, get_object_or_404
+from eshop.models import Product,Category
+from django.db.models import Q
+
+# Create your views here.
+
+
+def Searchresult(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query= request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query) | Q(desc__contains=query))
+    return render(request,"search.html",{'query':query,'products':products})
